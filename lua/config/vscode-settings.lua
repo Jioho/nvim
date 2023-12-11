7cd45da6700adf4d3419abd2f5cdef563723493d
陈志豪 <chenzhihao@myzaker.com>
2023-12-11 10:33:47 +0800

    add split command

--- lua/config/vscode-settings.lua
+++ lua/config/vscode-settings.lua
@@ -1,15 +1,18 @@
-local mode_nv = { "n", "x" }
+local mode_nv = { "n", "v" }
 local mode_v = { "v" }
 local mode_i = { "i" }
-local mode_nvi = { "n", "x", "i" }
+local mode_nvi = { "n", "v", "i" }
 
 
 local mappings = {
-  { from = '<C-j>', to = 'workbench.action.navigateDown',  mode = mode_nv },
-  { from = '<C-k>', to = 'workbench.action.navigateUp',    mode = mode_nv },
-  { from = '<C-h>', to = 'workbench.action.navigateLeft',  mode = mode_nv },
-  { from = '<C-l>', to = 'workbench.action.navigateRight', mode = mode_nv },
-  -- { from = '<C-b>', to = 'workbench.action.toggleSidebarVisibility', mode = mode_nvi },
+  { from = '<C-j>',     to = 'workbench.action.navigateDown',    mode = mode_nv },
+  { from = '<C-k>',     to = 'workbench.action.navigateUp',      mode = mode_nv },
+  { from = '<C-h>',     to = 'workbench.action.navigateLeft',    mode = mode_nv },
+  { from = '<C-l>',     to = 'workbench.action.navigateRight',   mode = mode_nv },
+
+  { from = '<leader>v', to = "workbench.action.splitEditor",     mode = mode_nvi },
+  { from = '<leader>s', to = "workbench.action.splitEditorDown", mode = mode_nvi }
+
 }
 
 -- vim.api.nvim_set_keymap('n', '<C-w>_', [[:lua require('vscode-neovim').call('workbench.action.toggleEditorWidths')<CR>]],
@@ -21,9 +24,6 @@ local mappings = {
 --   { noremap = true, silent = true })
 
 for _, mapping in ipairs(mappings) do
-  -- vim.api.nvim_set_keymap(mapping.mode or "n", mapping.from,
-  --   [[:lua vscode.call(']] .. mapping.to .. [[')<CR>]],
-  --   { noremap = true, silent = true })
   vim.keymap.set(mapping.mode or "n", mapping.from,
     [[:lua vscode.call(']] .. mapping.to .. [[')<CR>]],
     { noremap = true, silent = true })

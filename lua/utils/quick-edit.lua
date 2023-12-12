5ad895ca1fa120b5607e4dda5a2acebe0084bcd0
陈志豪 <chenzhihao@myzaker.com>
2023-12-12 19:04:50 +0800

    edit quick edit

--- lua/utils/quick-edit.lua
+++ lua/utils/quick-edit.lua
@@ -1,7 +1,7 @@
 local keyword = '<++>'
 function SearchAndReplace()
   vim.fn.search(keyword, 'w')
-  vim.api.nvim_command(':let @a=""\n')   -- 清空寄存器 a 的内容
+  vim.api.nvim_command(':let @a=""\n') -- 清空寄存器 a 的内容
   -- vim.api.nvim_command('normal! "_c4l')  -- 进入编辑模式并删除 4 个字符
   -- vim.api.nvim_command('startinsert')
 end
